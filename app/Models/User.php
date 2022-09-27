53bd4d39a12bc28cadb754e09a96023f24e1a93f
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-09-27 17:51:29 +0300

    Add view files and images for him, routes, seeds, factory, migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,9 +42,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-//    public function todos(): HasMany
-//    {
-//        return $this->hasMany(TodoItem::class);
-//    }
 }
