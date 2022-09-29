7f72fedf472b54eab0fc4f8631e37707530224a5
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-09-29 18:14:58 +0300

    add 'logo' column to 'listings' table, add create view, add nav paginate, update logo, add validate, add 2 routes

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -11,7 +12,7 @@ class ListingController extends Controller
     public function index()
     {
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
@@ -22,4 +23,34 @@ class ListingController extends Controller
             'listing' => $listing
         ]);
     }
+
+    // Show Create Form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // Store Listing Data
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Listing created successfully!');
+    }
+
+
 }
